abf94673614653af646803db20a90a52b507acde
maria lombardo <mariialombardo07@gmail.com>
2022-09-09 11:50:05 -0300

    viajes de react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <header>
+    <nav className="navbar navbar-dark bg-primary">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">Disfruta Viajando</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarText">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Inicio</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Destinos Para Invierno</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Destinos Para Verano</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Rebajas</a>
+        </li>
+      </ul>
+      <span className="navbar-text">
+        
+      </span>
+    </div>
+  </div>
+</nav>
+    </header>
     </div>
   );
 }
